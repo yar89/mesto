1379b74d438c2fb36ecb13d144a1e655ee7982d2
Iaroslava Tarchukova <yar89@yandex.ru>
2023-02-20 12:48:26 +0300

    fixed new bug

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ let jobInput = popup.querySelector('.popup__input_type-profession');
 let profileUserName = document.querySelector('.profile__name');
 let profileUserProfession = document.querySelector('.profile__description');
 
-let handleEditButtonClick = () => {
+let EditButtonClick = () => {
     popup.classList.add('popup_opened');
     nameInput.value = profileUserName.textContent;
     jobInput.value = profileUserProfession.textContent;
 }
 
-let handleCloseButtonClick = () => {
-    popup.classList.remove('popup_opened');
+let CloseButtonClick = () => {
+   popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileUserName.textContent = nameInput.value;
     profileUserProfession.textContent = jobInput.value;
-    handleCloseButtonClick();
+    CloseButtonClick();
 }
 
-editProfileButton.addEventListener('click', handleEditButtonClick);
-closeButton.addEventListener('click', handleCloseButtonClick);
+editProfileButton.addEventListener('click', EditButtonClick);
+closeButton.addEventListener('click', CloseButtonClick);
 popupForm.addEventListener('submit', handleFormSubmit);
 

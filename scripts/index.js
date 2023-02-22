792811f1e6f098f6a61b418ff34b0d101d25c0dc
Iaroslava Tarchukova <yar89@yandex.ru>
2023-02-22 09:55:47 +0300

    changes after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 // Находим форму в DOM
-let editProfileButton = document.querySelector('.profile__edit-button'); 
+let profileEditButton = document.querySelector('.profile__edit-button'); 
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close');
 let popupForm = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__input_type-name');
-let jobInput = popup.querySelector('.popup__input_type-profession');
+let nameInput = popup.querySelector('.popup__input_type_profile-name');
+let jobInput = popup.querySelector('.popup__input_type_profile-profession');
 let profileUserName = document.querySelector('.profile__name');
 let profileUserProfession = document.querySelector('.profile__description');
 
-let EditButtonClick = () => {
+let handleEditButtonClick = () => {
     popup.classList.add('popup_opened');
     nameInput.value = profileUserName.textContent;
     jobInput.value = profileUserProfession.textContent;
 }
 
-let CloseButtonClick = () => {
+let handleCloseButtonClick = () => {
    popup.classList.remove('popup_opened');
 }
 
@@ -22,10 +22,10 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileUserName.textContent = nameInput.value;
     profileUserProfession.textContent = jobInput.value;
-    CloseButtonClick();
+    handleCloseButtonClick();
 }
 
-editProfileButton.addEventListener('click', EditButtonClick);
-closeButton.addEventListener('click', CloseButtonClick);
+profileEditButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
 popupForm.addEventListener('submit', handleFormSubmit);
 

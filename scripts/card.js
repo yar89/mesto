69bd27579f810d3b1fb5216c6a33d219969b60c7
Iaroslava Tarchukova <yar89@yandex.ru>
2023-04-27 09:37:50 +0300

    changes after first review

--- scripts/card.js
+++ scripts/card.js
@@ -1,72 +1,69 @@
 export class Card {
-    constructor(data, templateSelector, openPopupLargeImage) {
-      this._link = data.link;
-      this._name = data.name;
-      this._templateSelector = templateSelector;
-      this._openPopupLargeImage = openPopupLargeImage;
-    }
-  
-    _getTemplate(){
-      //забираем разметку из HTML и клонируем элемент
-      const cardElement = document
+  constructor(data, templateSelector, openPopupLargeImage) {
+    this._link = data.link;
+    this._name = data.name;
+    this._templateSelector = templateSelector;
+    this._openPopupLargeImage = openPopupLargeImage;
+  }
+
+  _getTemplate() {
+    //забираем разметку из HTML и клонируем элемент
+    const cardElement = document
       .querySelector(this._templateSelector)
-      .content
-      .querySelector('.card')
+      .content.querySelector(".card")
       .cloneNode(true);
-  
-      //вернем  DOM-элемент карточки
-      return cardElement;
-    }
-  
-    //Публичный метод подготовит карточку к публикации
-    generateCard(){
-      //запишем разметку в приватное поле _element. Так у других эелеинтов появится доступ к ней
-      this._element = this._getTemplate();
-      
-      //Добавим данные
-      this._imageElement = this._element.querySelector('.card__image');
-      this._likeButtonElement = this._element.querySelector('.card__like-button');
-      this._deliteButtonElement = this._element.querySelector('.card__delite-button');
-      this._cardTitle = this._element.querySelector('.card__title');
-  
-      this._imageElement.src = this._link;
-      this._imageElement.alt = this._name;
-      this._cardTitle.textContent = this._name;
-  
-      this._setEventListeners();
-  
-      //Вернем элемент наружу
-      return this._element;
-    }
-  
-    
-    _likeCard() {
-      this._likeButtonElement.classList.toggle('card__like-button_active');
-    };
-  
-    _deliteCard(event) {
-      this._deliteButtonElement = event.target.closest('.card');
-      this._deliteButtonElement.remove();
-      
-    }
-  
-    _openLargeImage() {
-      this._openPopupLargeImage(this._name, this._link);
-      
-    }
-  
-    _setEventListeners() {
-      this._likeButtonElement.addEventListener('click', () => {
-        this._likeCard();
-      });
-      
-      this._deliteButtonElement.addEventListener('click', (event) => {
-        this._deliteCard(event);
-      });
-  
-      this._imageElement.addEventListener('click', () => {
-        this._openLargeImage();
-      })
-  
-    }
-  };
+
+    //вернем  DOM-элемент карточки
+    return cardElement;
+  }
+
+  //Публичный метод подготовит карточку к публикации
+  generateCard() {
+    //запишем разметку в приватное поле _element. Так у других эелеинтов появится доступ к ней
+    this._element = this._getTemplate();
+
+    //Добавим данные
+    this._imageElement = this._element.querySelector(".card__image");
+    this._likeButtonElement = this._element.querySelector(".card__like-button");
+    this._deleteButtonElement = this._element.querySelector(
+      ".card__delite-button"
+    );
+    this._cardTitle = this._element.querySelector(".card__title");
+
+    this._imageElement.src = this._link;
+    this._imageElement.alt = this._name;
+    this._cardTitle.textContent = this._name;
+
+    this._setEventListeners();
+
+    //Вернем элемент наружу
+    return this._element;
+  }
+
+  _likeCard() {
+    this._likeButtonElement.classList.toggle("card__like-button_active");
+  }
+
+  _deleteCard(event) {
+    this._deleteButtonElement = event.target.closest(".card");
+    this._element.remove();
+  }
+
+  _openLargeImage() {
+    this._openPopupLargeImage(this._name, this._link);
+  }
+
+  _setEventListeners() {
+    this._likeButtonElement.addEventListener("click", () => {
+      this._likeCard();
+    });
+
+    this._deleteButtonElement.addEventListener("click", (event) => {
+      this._deleteCard(event);
+    });
+
+    this._imageElement.addEventListener("click", () => {
+      this._openLargeImage();
+    });
+  }
+}
---
+++ scripts/constants.js
@@ -0,0 +1,36 @@
+//начальный массив изображений
+export const initialCards = [
+  {
+    name: "Москва",
+    link: "https://images.unsplash.com/photo-1547448415-e9f5b28e570d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Красноярск",
+    link: "https://images.unsplash.com/photo-1587451152235-05466c2fc532?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Камчатка",
+    link: "https://images.unsplash.com/photo-1557094005-176cbfe3554d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1334&q=80",
+  },
+  {
+    name: "Петергоф",
+    link: "https://images.unsplash.com/photo-1610197361600-33a3a5073cad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Алтайский край",
+    link: "https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2067&q=80",
+  },
+  {
+    name: "Байкал",
+    link: "https://images.unsplash.com/photo-1548130729-90d4d11826f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
+
+export const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit-button",
+  inactiveButtonClass: "popup__submit-button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
